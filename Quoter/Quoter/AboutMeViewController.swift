4081f313d2da6dc00743f85a2239c55b3c995816
Mehmet Dilaveroğlu <mdilaveroglu@gmail.com>
2016-03-07 15:49:17 +0200

    About me page completed.

--- Quoter/Quoter/AboutMeViewController.swift
+++ Quoter/Quoter/AboutMeViewController.swift
@@ -13,10 +13,29 @@ import MMDrawerController
 class AboutMeViewController: UIViewController {
 
     @IBOutlet weak var navigationDrawerButton: UIBarButtonItem!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    @IBOutlet weak var twitterButton: UIButton!
+    
+    @IBOutlet weak var linkedinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationDrawerButton.setFAIcon(FAType.FANavicon, iconSize: 25)
+        facebookButton.setFAIcon(FAType.FAFacebook, forState: .Normal)
+        twitterButton.setFAIcon(FAType.FATwitter, forState: .Normal)
+        linkedinButton.setFAIcon(FAType.FALinkedin, forState: .Normal)
+        
+        image.layer.borderWidth = 1
+        image.layer.borderColor = UIColor(red: 0, green: 150.0 / 255.0 , blue: 136.0 / 255.0, alpha: 1).CGColor
+        image.layer.cornerRadius = image.frame.size.width / 2
+        image.clipsToBounds = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +48,50 @@ class AboutMeViewController: UIViewController {
         let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.centerContainer!.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
     }
+    
+    @IBAction func emailTapped(sender: UIButton) {
+        let url = NSURL(string: "mailto:mdilaveroglu@gmail.com")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    @IBAction func facebookTapped(sender: UIButton) {
+        let webURL = NSURL(string: "https://www.facebook.com/mdilaveroglu")!
+        let appURL = NSURL(string:"fb://profile/mdilaveroglu")!
+        
+        if(UIApplication.sharedApplication().canOpenURL(appURL)){
+            // FB installe
+            UIApplication.sharedApplication().openURL(appURL)
+        } else {
+            // FB is not installed, open in safari
+            UIApplication.sharedApplication().openURL(webURL)
+        }
+    }
 
+    @IBAction func twitterTapped(sender: UIButton) {
+        let screenName =  "mdilaverm"
+        let appURL = NSURL(string: "twitter://user?screen_name=\(screenName)")!
+        let webURL = NSURL(string: "https://twitter.com/\(screenName)")!
+        
+        let application = UIApplication.sharedApplication()
+        
+        if application.canOpenURL(appURL) {
+            application.openURL(appURL)
+        } else {
+            application.openURL(webURL)
+        }
+    }
 
+    @IBAction func linkedinTapped(sender: UIButton) {
+        let profileName =  "mdilaveroglu"
+        let appURL = NSURL(string: "linkedin://profile/\(profileName)")!
+        let webURL = NSURL(string: "https://linkedin.com/in/\(profileName)")!
+        
+        let application = UIApplication.sharedApplication()
+        
+        if application.canOpenURL(appURL) {
+            application.openURL(appURL)
+        } else {
+            application.openURL(webURL)
+        }
+    }
 }
